e94c920e8a5dd9c204096ce85fc618f9eddd95b5
AndresPaucar7 <nmapfr@gmail.com>
2025-03-04 03:05:59 -0500

    Initial commit

---
+++ src/main/java/com/example/TravelBloggerServices/TravelBloggerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravelBloggerServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBloggerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBloggerServicesApplication.class, args);
+	}
+
+}
